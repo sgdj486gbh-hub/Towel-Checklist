37f58e6f00b24eeabe8600f7ece4f8d95fa45b81
sgdj486gbh-hub <sgdj486gbh@privaterelay.appleid.com>
2025-11-27 09:28:36 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
-// Simple service worker placeholder
+const CACHE_NAME = 'towel-checklist-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+self.addEventListener('install', ev => {
+  ev.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', ev => {
+  ev.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', ev => {
+  ev.respondWith(
+    caches.match(ev.request).then(r => r || fetch(ev.request))
+  );
+});
